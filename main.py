0d596c058c2833b308331216105c836034874e5e
Vykunth Rao <stingrose@gmail.com>
2023-04-24 19:00:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ def predict():
         return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
